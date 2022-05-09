afffddd7ec43bc65b5db8c5dbe09ef5feba78e07
johnsonhung906 <khhung906@gmail.com>
2022-05-09 19:00:42 +0800

    update to autoencoder version

--- meta.py
+++ meta.py
@@ -6,8 +6,10 @@ from    torch.utils.data import TensorDataset, DataLoader
 from    torch import optim
 import  numpy as np
 
-from    learner import conv_autoencoder
+from    learner import Learner
 from    copy import deepcopy
+
+from    sklearn.metrics import roc_auc_score
 
 
 
@@ -30,33 +32,28 @@ class Meta(nn.Module):
         self.update_step = args.update_step
         self.update_step_test = args.update_step_test
 
-        if model == 'conv_autoencoder':
-            self.net = conv_autoencoder(args.imgc, args.imgsz)
+        self.net = Learner(model, args.imgc, args.imgsz)
     
         self.meta_optim = optim.Adam(self.net.parameters(), lr=self.meta_lr)
         self.loss_fn = F.mse_loss
 
-    def forward(self, x_spt, y_spt, x_qry, y_qry):
+    def forward(self, x_spt, x_qry):
         """
 
         :param x_spt:   [b, setsz, c_, h, w]
-        :param y_spt:   [b, setsz]
         :param x_qry:   [b, querysz, c_, h, w]
-        :param y_qry:   [b, querysz]
         :return:
         """
         task_num, setsz, c_, h, w = x_spt.size()
         querysz = x_qry.size(1)
 
         losses_q = [0 for _ in range(self.update_step + 1)]  # losses_q[i] is the loss on step i
-        corrects = [0 for _ in range(self.update_step + 1)]
-
 
         for i in range(task_num):
 
             # 1. run the i-th task and compute loss for k=0
             logits = self.net(x_spt[i], bn_training=True)
-            loss = self.loss_fn(logits, y_spt[i])
+            loss = self.loss_fn(logits, x_spt[i])
             grad = torch.autograd.grad(loss, self.net.parameters())
             fast_weights = list(map(lambda p: p[1] - self.update_lr * p[0], zip(grad, self.net.parameters())))
 
@@ -64,28 +61,24 @@ class Meta(nn.Module):
             with torch.no_grad():
                 # [setsz, nway]
                 logits_q = self.net(x_qry[i], self.net.parameters(), bn_training=True)
-                loss_q = self.loss_fn(logits_q, y_qry[i])
+                loss_q = self.loss_fn(logits_q, x_qry[i])
                 losses_q[0] += loss_q
 
-                pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
-                correct = torch.eq(pred_q, y_qry[i]).sum().item()
-                corrects[0] = corrects[0] + correct
+                # pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
 
             # this is the loss and accuracy after the first update
             with torch.no_grad():
                 # [setsz, nway]
                 logits_q = self.net(x_qry[i], fast_weights, bn_training=True)
-                loss_q = self.loss_fn(logits_q, y_qry[i])
+                loss_q = self.loss_fn(logits_q, x_qry[i])
                 losses_q[1] += loss_q
                 # [setsz]
-                pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
-                correct = torch.eq(pred_q, y_qry[i]).sum().item()
-                corrects[1] = corrects[1] + correct
+                # pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
 
             for k in range(1, self.update_step):
                 # 1. run the i-th task and compute loss for k=1~K-1
                 logits = self.net(x_spt[i], fast_weights, bn_training=True)
-                loss = self.loss_fn(logits, y_spt[i])
+                loss = self.loss_fn(logits, x_spt[i])
                 # 2. compute grad on theta_pi
                 grad = torch.autograd.grad(loss, fast_weights)
                 # 3. theta_pi = theta_pi - train_lr * grad
@@ -93,19 +86,15 @@ class Meta(nn.Module):
 
                 logits_q = self.net(x_qry[i], fast_weights, bn_training=True)
                 # loss_q will be overwritten and just keep the loss_q on last update step.
-                loss_q = self.loss_fn(logits_q, y_qry[i])
+                loss_q = self.loss_fn(logits_q, x_qry[i])
                 losses_q[k + 1] += loss_q
-
-                with torch.no_grad():
-                    pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
-                    correct = torch.eq(pred_q, y_qry[i]).sum().item()  # convert to numpy
-                    corrects[k + 1] = corrects[k + 1] + correct
-
 
 
         # end of all tasks
         # sum over all losses on query set across all tasks
         loss_q = losses_q[-1] / task_num
+
+        # print(loss_q)
 
         # optimize theta parameters
         self.meta_optim.zero_grad()
@@ -116,25 +105,19 @@ class Meta(nn.Module):
         self.meta_optim.step()
 
 
-        accs = np.array(corrects) / (querysz * task_num)
-
-        return accs
+        return loss_q.detach().cpu().numpy()[0]
 
 
-    def finetunning(self, x_spt, y_spt, x_qry, y_qry):
+    def finetunning(self, x_spt, x_qry):
         """
 
         :param x_spt:   [setsz, c_, h, w]
-        :param y_spt:   [setsz]
         :param x_qry:   [querysz, c_, h, w]
-        :param y_qry:   [querysz]
         :return:
         """
         assert len(x_spt.shape) == 4
 
         querysz = x_qry.size(0)
-
-        corrects = [0 for _ in range(self.update_step_test + 1)]
 
         # in order to not ruin the state of running_mean/variance and bn_weight/bias
         # we finetunning on the copied model instead of self.net
@@ -142,7 +125,7 @@ class Meta(nn.Module):
 
         # 1. run the i-th task and compute loss for k=0
         logits = net(x_spt)
-        loss = self.loss_fn(logits, y_spt)
+        loss = self.loss_fn(logits, x_spt)
         grad = torch.autograd.grad(loss, net.parameters())
         fast_weights = list(map(lambda p: p[1] - self.update_lr * p[0], zip(grad, net.parameters())))
 
@@ -151,25 +134,19 @@ class Meta(nn.Module):
             # [setsz, nway]
             logits_q = net(x_qry, net.parameters(), bn_training=True)
             # [setsz]
-            pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
-            # scalar
-            correct = torch.eq(pred_q, y_qry).sum().item()
-            corrects[0] = corrects[0] + correct
+            # pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
 
         # this is the loss and accuracy after the first update
         with torch.no_grad():
             # [setsz, nway]
             logits_q = net(x_qry, fast_weights, bn_training=True)
             # [setsz]
-            pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
-            # scalar
-            correct = torch.eq(pred_q, y_qry).sum().item()
-            corrects[1] = corrects[1] + correct
+            # pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
 
         for k in range(1, self.update_step_test):
             # 1. run the i-th task and compute loss for k=1~K-1
             logits = net(x_spt, fast_weights, bn_training=True)
-            loss = self.loss_fn(logits, y_spt)
+            loss = self.loss_fn(logits, x_spt)
             # 2. compute grad on theta_pi
             grad = torch.autograd.grad(loss, fast_weights)
             # 3. theta_pi = theta_pi - train_lr * grad
@@ -177,16 +154,10 @@ class Meta(nn.Module):
 
             logits_q = net(x_qry, fast_weights, bn_training=True)
             # loss_q will be overwritten and just keep the loss_q on last update step.
-            loss_q = self.loss_fn(logits_q, y_qry)
+            loss_q = self.loss_fn(logits_q, x_qry)
 
-            with torch.no_grad():
-                pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
-                correct = torch.eq(pred_q, y_qry).sum().item()  # convert to numpy
-                corrects[k + 1] = corrects[k + 1] + correct
-
-
+        # print(loss_q)
         del net
 
-        accs = np.array(corrects) / querysz
 
-        return accs
+        return loss_q.cpu().detach().numpy()[0]
--- meta_train.py
+++ meta_train.py
@@ -7,14 +7,15 @@ from    torch.optim import lr_scheduler
 import  random, sys, pickle
 import  argparse
 
+from MiniImagenet import MiniImagenet
 from meta import Meta
 
 
-def mean_confidence_interval(accs, confidence=0.95):
-    n = accs.shape[0]
-    m, se = np.mean(accs), scipy.stats.sem(accs)
-    h = se * scipy.stats.t._ppf((1 + confidence) / 2, n - 1)
-    return m, h
+# def mean_confidence_interval(accs, confidence=0.95):
+#     n = accs.shape[0]
+#     m, se = np.mean(accs), scipy.stats.sem(accs)
+#     h = se * scipy.stats.t._ppf((1 + confidence) / 2, n - 1)
+#     return m, h
 
 
 def main():
@@ -34,19 +35,27 @@ def main():
     print('Total trainable tensors:', num)
 
     # batchsz here means total episode number
-    train_data = MVTec(args.data_root, 
-                mode='train', 
-                k_shot=args.k_spt,
-                k_query=args.k_qry,
-                resize=args.imgsz,
-                batchsz=10000)
+    # train_data = MVTec(args.data_root, 
+    #             mode='train', 
+    #             k_shot=args.k_spt,
+    #             k_query=args.k_qry,
+    #             resize=args.imgsz,
+    #             batchsz=10000)
 
-    test_data = MVTec(args.data_root, 
-                mode='test', 
-                k_shot=args.k_spt,
-                k_query=args.k_qry,
-                resize=args.imgsz,
-                batchsz=100)
+    # test_data = MVTec(args.data_root, 
+    #             mode='test', 
+    #             k_shot=args.k_spt,
+    #             k_query=args.k_qry,
+    #             resize=args.imgsz,
+    #             batchsz=100)
+
+    train_data = MiniImagenet(root='../MAML-Pytorch/miniimagenet/', mode='train', n_way=1, k_shot=args.k_spt,
+                        k_query=args.k_qry,
+                        batchsz=10000, resize=args.imgsz)
+
+    test_data = MiniImagenet(root='../MAML-Pytorch/miniimagenet/', mode='test', n_way=1, k_shot=args.k_spt,
+                             k_query=args.k_qry,
+                             batchsz=100, resize=args.imgsz)
 
     for epoch in range(args.epoch//10000):
         # fetch meta_batchsz num of episode each time
@@ -55,26 +64,27 @@ def main():
         for step, (x_spt, y_spt, x_qry, y_qry) in enumerate(db):
 
             x_spt, y_spt, x_qry, y_qry = x_spt.to(device), y_spt.to(device), x_qry.to(device), y_qry.to(device)
+            # print(x_spt.size(), y_spt.size(), x_qry.size() ,y_qry.size())
 
-            accs = maml(x_spt, y_spt, x_qry, y_qry)
+            loss = maml(x_spt, x_qry)
 
             if step % 30 == 0:
-                print('step:', step, '\ttraining acc:', accs)
+                print('step:', step, '\ttraining loss:', loss)
 
             if step % 500 == 0:  # evaluation
                 db_test = DataLoader(test_data, 1, shuffle=True, num_workers=1, pin_memory=True)
-                accs_all_test = []
+                loss_all_test = []
 
                 for x_spt, y_spt, x_qry, y_qry in db_test:
                     x_spt, y_spt, x_qry, y_qry = x_spt.squeeze(0).to(device), y_spt.squeeze(0).to(device), \
                                                  x_qry.squeeze(0).to(device), y_qry.squeeze(0).to(device)
 
-                    accs = maml.finetunning(x_spt, y_spt, x_qry, y_qry)
-                    accs_all_test.append(accs)
+                    loss = maml.finetunning(x_spt, x_qry)
+                    loss_all_test.append(loss)
 
                 # [b, update_step+1]
-                accs = np.array(accs_all_test).mean(axis=0).astype(np.float16)
-                print('Test acc:', accs)
+                loss = np.array(loss_all_test).mean(axis=0).astype(np.float16)
+                print('Test loss:', loss)
 
 
 if __name__ == '__main__':
@@ -83,7 +93,7 @@ if __name__ == '__main__':
     argparser.add_argument('--data_root', type=str, help='root dir of dataset', default='../data/MVTec')
     argparser.add_argument('--epoch', type=int, help='epoch number', default=60000)
     # argparser.add_argument('--n_way', type=int, help='n way', default=5)
-    argparser.add_argument('--k_spt', type=int, help='k shot for support set', default=1)
+    argparser.add_argument('--k_spt', type=int, help='k shot for support set', default=10)
     argparser.add_argument('--k_qry', type=int, help='k shot for query set', default=15)
     argparser.add_argument('--imgsz', type=int, help='imgsz', default=84)
     argparser.add_argument('--imgc', type=int, help='imgc', default=3)
